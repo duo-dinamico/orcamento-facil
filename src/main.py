111a869167fd8106a1cf1deb6d1e4433dc11fb5f
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2023-10-20 08:42:43 +0100

    melhoria da class

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 
-class MainWindow:
-    def __init__(self, root):
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
         # Constants
         WINDOW_WIDTH = 1200
         WINDOW_HEIGHT = 800
 
-        root.title("Orçamento Fácil")
+        self.wm_title("Orçamento Fácil")
 
-        root.resizable(False, False)  # Prevent resizing ???
-        x_center = root.winfo_screenwidth() / 2 - WINDOW_WIDTH / 2
-        y_center = root.winfo_screenheight() / 2 - WINDOW_HEIGHT / 2
-        root.geometry(
+        self.wm_resizable(False, False)  # Prevent resizing ???
+        x_center = self.winfo_screenwidth() / 2 - WINDOW_WIDTH / 2
+        y_center = self.winfo_screenheight() / 2 - WINDOW_HEIGHT / 2
+        self.geometry(
             f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{int(x_center)}+{int(y_center)}"
         )  # Put the windows in the center of the screen
 
-        message = ttk.Label(root, text="Hello, World!")
+        message = ttk.Label(self, text="Hello, World!")
         message.pack()
 
 
 if __name__ == "__main__":
-    root = Tk()
-    MainWindow(root)
+    root = MainWindow()
     root.mainloop()

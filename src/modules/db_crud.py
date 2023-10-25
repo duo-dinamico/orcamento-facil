e04939435cf30384db395da9e92a9e3f418e2963
João Carlos Silva <jcvsilva@gmail.com>
2023-10-25 17:22:00 +0100

    Improving db.

--- src/modules/db_crud.py
+++ src/modules/db_crud.py
@@ -1,12 +1,26 @@
+from sqlalchemy import select
+
 from modules.db_models import User
 
 from modules.db_database import SessionLocal
 
 
 def create_user(db: SessionLocal, user_name: str, user_password: str) -> User:
+    exist = db.scalars(select(User).where(User.username == user_name)).first()
+    print(exist)
+    if exist:
+        print("User already exist.")
+        return False
+
     db_user = User(username=user_name, password=user_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     print(f"create_user will return: {db_user}")
-    return db_user
+    return True
+
+
+def read_user(db: SessionLocal, user_name: str, user_password: str) -> User:
+    return db.scalars(
+        select(User).where(User.username == user_name, User.password == user_password)
+    ).first()
--- src/modules/db_models.py
+++ src/modules/db_models.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from datetime import datetime
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
@@ -25,6 +26,14 @@ class AccountTypeEnum(str, Enum):
     CASH = "Cash"
 
 
+# Define an enum type for recurrency
+class RecurrencyEnum(str, Enum):
+    ONE = "One time only"
+    DAY = "Daily"
+    MONTH = "Monthly"
+    YEAR = "Yearly"
+
+
 class Account(Base):
     __tablename__ = "accounts"
 
@@ -36,6 +45,47 @@ class Account(Base):
     initial_balance: Mapped[int] = mapped_column(default=0)  # In cents
     balance: Mapped[int] = mapped_column(default=0)  # In cents
     credit_limit: Mapped[Optional[int]]  # In cents
-    payment_day: Mapped[Optional[str]]
+    payment_day: Mapped[Optional[str]]  # Saved as a string, need conversion
     interest_rate: Mapped[Optional[float]]
     credit_method: Mapped[Optional[str]]
+
+
+class Income(Base):
+    __tablename__ = "incomes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    account_id: Mapped[int] = mapped_column(ForeignKey("accounts.id", name="account"))
+    name: Mapped[str] = mapped_column(unique=True)
+    expected_income_value: Mapped[int] = mapped_column(default=0)  # In cents
+    real_income_value: Mapped[int] = mapped_column(default=0)  # In cents
+    income_day: Mapped[Optional[str]]  # Saved as a string, need conversion
+    recurrency: Mapped[RecurrencyEnum]
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    account_id: Mapped[int] = mapped_column(ForeignKey("accounts.id", name="account"))
+    subcategory_id: Mapped[int] = mapped_column(ForeignKey("subcategories.id", name="subcategory"))
+    date: Mapped[datetime]
+    value: Mapped[int] = mapped_column(default=0)  # In cents
+    description: Mapped[Optional[str]]
+
+
+class SubCategories(Base):
+    __tablename__ = "subcategories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", name="category"))
+    name: Mapped[str] = mapped_column(unique=True)
+    recurrent: Mapped[bool] = mapped_column(default=False)
+    recurrency: Mapped[RecurrencyEnum]
+    include: Mapped[bool] = mapped_column(default=True)
+
+
+class Categories(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)

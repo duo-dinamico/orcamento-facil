7c088f8cbd609eecae26b3e6dbd97cefa3ca936c
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2023-11-01 19:29:17 +0000

    adicionar fontes de rendimento

--- src/modules/frame_starting_page.py
+++ src/modules/frame_starting_page.py
@@ -10,13 +10,19 @@ from modules.db_crud import (
 )
 
 
+def popup_setup(self, parent, title):
+    # Make sure you're not able to interact with the main window
+    self.grab_set()
+    self.transient(parent)
+    self.resizable(False, False)
+    self.title(title)
+
+
 class AddAccountPopUp(tk.Toplevel):
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.controller = controller
-        self.resizable(False, False)
-        self.title("Add account")
+        popup_setup(self, parent, "Add account")
 
         self.fields = []
         self.account_name = tk.StringVar()
@@ -45,7 +51,7 @@ class AddAccountPopUp(tk.Toplevel):
         self.fields.append(
             ttk.Label(
                 frame_popup,
-                text="Currency (€ or £)",
+                text="Currency",
             )
         )
         self.currency_combobox = ttk.Combobox(frame_popup, state="readonly", values=self.currencies)
@@ -90,12 +96,18 @@ class AddIncomePopUp(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.resizable(False, False)
-        self.title("Add income source")
+        popup_setup(self, parent, "Add income source")
 
         self.fields = []
         self.income_source_name = tk.StringVar()
-        self.income_source_name.trace_add("write", self.activate_add_account)
+        self.income_source_name.trace_add("write", self.activate_add_income)
+        self.predicted_income = tk.StringVar()
+        self.predicted_income.trace_add("write", self.activate_add_income)
+        self.income_date_day = tk.StringVar(value="Day")
+        self.income_date_month = tk.StringVar(value="Month")
+        self.income_date_year = tk.StringVar(value="Year")
+        self.income_frequency_value = tk.IntVar(value=1)
+        self.income_frequency = ["Day", "Month", "Year"]
 
         frame_popup = ttk.Frame(self)
         frame_popup.pack(anchor="nw", fill="both", expand=True)
@@ -107,6 +119,96 @@ class AddIncomePopUp(tk.Toplevel):
             )
         )
         self.fields.append(ttk.Entry(frame_popup, textvariable=self.income_source_name))
+        self.fields.append(
+            ttk.Label(
+                frame_popup,
+                text="Predicted income",
+            )
+        )
+        self.fields.append(ttk.Entry(frame_popup, textvariable=self.predicted_income))
+        self.fields.append(
+            ttk.Label(
+                frame_popup,
+                text="Income date (dd/mm/yyyy)",
+            )
+        )
+        self.fields.append(ttk.Frame(frame_popup))
+
+        for field in self.fields:
+            field.pack(anchor="w", padx=10, pady=5, fill="x")
+
+        ttk.Entry(self.fields[-1], textvariable=self.income_date_day).pack(
+            side="left",
+            expand=True,
+            fill="both",
+        )
+        ttk.Entry(self.fields[-1], textvariable=self.income_date_month).pack(
+            side="left",
+            padx=(10, 10),
+            expand=True,
+            fill="both",
+        )
+        ttk.Entry(self.fields[-1], textvariable=self.income_date_year).pack(
+            side="left",
+            expand=True,
+            fill="both",
+        )
+
+        ttk.Label(
+            frame_popup,
+            text="Income frequency",
+        ).pack(anchor="w", padx=10, pady=5, fill="x")
+        self.frequency_frame = ttk.Frame(self)
+        self.frequency_frame.pack(anchor="w", padx=10, pady=5, fill="x")
+        ttk.Label(self.frequency_frame, text="Every ").pack(
+            side="left",
+            expand=True,
+            fill="both",
+        )
+        ttk.Entry(self.frequency_frame, textvariable=self.income_frequency_value).pack(
+            side="left",
+            padx=(10, 10),
+            expand=True,
+            fill="both",
+        )
+        self.frequency_combobox = ttk.Combobox(
+            self.frequency_frame, state="readonly", values=self.income_frequency
+        )
+        self.frequency_combobox.current(1)
+        self.frequency_combobox.pack(
+            side="left",
+            expand=True,
+            fill="both",
+        )
+
+        ttk.Button(
+            self,
+            text="Cancel",
+            command=self.cancel_input,
+        ).pack(side="left", padx=10, pady=5)
+        self.add_income = ttk.Button(
+            self,
+            text="Add income",
+            state="disabled",
+            command=lambda: self.parent.add_income(
+                {
+                    "income": self.income_source_name.get(),
+                    "predicted_income": int(self.predicted_income.get()),
+                }
+            ),
+        )
+        self.add_income.pack(side="right", padx=10, pady=5)
+
+    def cancel_input(self):
+        self.destroy()
+
+    def activate_add_income(self, *args):
+        income_name = self.income_source_name.get()
+        predicted_income = self.predicted_income.get()
+        if income_name != "" and predicted_income != 0:
+            self.add_income.config(state="enabled")
+        else:
+            self.add_income.config(state="disabled")
 
 
 class StartingPage(ttk.Frame):
@@ -114,15 +216,16 @@ class StartingPage(ttk.Frame):
         ttk.Frame.__init__(self, parent)
         self.controller = controller
         self.fields = []
-        self.popup_open = False
         self.columnconfigure(0, weight=2)
         self.columnconfigure(1, weight=3)
         self.rowconfigure(0, weight=1)
         self.paddings = {"padx": 10, "pady": 5}
         self.accounts = []
+        self.incomes = []
         self.style = ttk.Style()
         self.style.configure("Red.TButton", background="red4", relief="raised")
         self.line_widgets = []
+        self.line_widgets_incomes = []
         self.frames_left = {}
         self.frames_right = {}
 
@@ -136,6 +239,7 @@ class StartingPage(ttk.Frame):
         self.frames_right["account_list"] = ttk.LabelFrame(
             self, text="Account list", relief="ridge"
         )
+        self.frames_right["income_list"] = ttk.LabelFrame(self, text="Income list", relief="ridge")
         for index, frame in enumerate(self.frames_left.values()):
             frame.grid(**self.paddings, column=0, row=index, sticky="new")
             frame.rowconfigure(index, weight=1)
@@ -210,6 +314,15 @@ class StartingPage(ttk.Frame):
         for index, title in enumerate(self.titles):
             title.grid(**self.paddings, column=index, row=0, sticky="new")
 
+        self.titles_income = []
+        self.titles_income.append(ttk.Label(self.frames_right["income_list"], text="Income name"))
+        self.titles_income.append(
+            ttk.Label(self.frames_right["income_list"], text="Predicted income")
+        )
+        self.titles_income.append(ttk.Label(self.frames_right["income_list"], text="Delete"))
+        for index, title in enumerate(self.titles_income):
+            title.grid(**self.paddings, column=index, row=0, sticky="new")
+
         ttk.Button(
             self,
             text="Seguinte",
@@ -217,6 +330,7 @@ class StartingPage(ttk.Frame):
         ).grid(column=0, row=3, columnspan=2, pady=5, padx=5, sticky="ew")
 
         self.refresh_accounts()
+        self.refresh_incomes()
 
     def add_user(self):
         state = create_user(self.controller.session, self.nome.get(), self.password.get())
@@ -256,9 +370,7 @@ class StartingPage(ttk.Frame):
             self.add_income_button.config(state="normal")
 
     def open_popup(self, popup):
-        if not self.popup_open:
-            self.popup = popup(self, self.controller)
-            self.popup_open = True
+        self.popup = popup(self)
 
     def add_account(self, account):
         created_id = create_account(
@@ -272,8 +384,12 @@ class StartingPage(ttk.Frame):
         print(f"add_account: {account}")
         self.accounts.append(account)
         self.popup.destroy()
-        self.popup_open = False
         self.refresh_accounts()
+
+    def add_income(self, income):
+        self.incomes.append(income)
+        self.popup.destroy()
+        self.refresh_incomes()
 
     def refresh_accounts(self):
         for widget in self.line_widgets:
@@ -304,6 +420,34 @@ class StartingPage(ttk.Frame):
                 self.line_widgets.append(balance_label)
                 self.line_widgets.append(delete_button)
 
+    def refresh_incomes(self):
+        for widget in self.line_widgets_incomes:
+            widget.destroy()
+
+        for i, item in enumerate(self.incomes):
+            income_name_label = ttk.Label(self.frames_right["income_list"], text=item["income"])
+            predicted_income_label = ttk.Label(
+                self.frames_right["income_list"], text=item["predicted_income"]
+            )
+            delete_button = ttk.Button(
+                self.frames_right["income_list"],
+                width=4,
+                style="Red.TButton",
+                command=lambda i=i: self.delete_income(i),
+            )
+
+            income_name_label.grid(**self.paddings, column=0, row=i + 1, sticky="new")
+            predicted_income_label.grid(**self.paddings, column=1, row=i + 1, sticky="new")
+            delete_button.grid(**self.paddings, column=2, row=i + 1)
+
+            self.line_widgets_incomes.append(income_name_label)
+            self.line_widgets_incomes.append(predicted_income_label)
+            self.line_widgets_incomes.append(delete_button)
+
     def delete_account(self, account_id):
         delete_account(self.controller.session, account_id, self.controller.logged_in)
         self.refresh_accounts()
+
+    def delete_income(self, income_id):
+        del self.incomes[income_id]
+        self.refresh_incomes()

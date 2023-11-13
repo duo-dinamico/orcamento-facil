37c97051d0a3c818ae7d6265d5f4811ddc675fd4
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2023-11-13 23:24:07 +0000

    resolve imports

--- src/ezbudget/main.py
+++ src/ezbudget/main.py
@@ -1,6 +1,6 @@
-from .model import Model
-from .presenter import Presenter
-from .view import RootView
+from model import Model
+from presenter import Presenter
+from view import RootView
 
 
 def main() -> None:
--- src/ezbudget/model/__init__.py
+++ src/ezbudget/model/__init__.py
@@ -1,2 +1,1 @@
-from .base_models import Account, AccountTypeEnum, Category, Income, MonthEnum, RecurrencyEnum, SubCategory, Transaction, User
 from .model import Model
